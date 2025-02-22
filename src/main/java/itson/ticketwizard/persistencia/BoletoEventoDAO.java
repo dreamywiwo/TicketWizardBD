f5ec3a413d979d6a966070bb6cf9bd754e001cf1
dreamywiwo <danacg11@hotmail.com>
2025-02-21 21:58:05 -0700

    Creacion Boleto EventoDAO

---
+++ src/main/java/itson/ticketwizard/persistencia/BoletoEventoDAO.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package itson.ticketwizard.persistencia;
+
+import itson.ticketwizard.dtos.BoletoEventoDTO;
+import java.util.List;
+
+/**
+ *
+ * @author Dana Chavez
+ */
+public class BoletoEventoDAO {
+    
+    public List<BoletoEventoDTO> obtenerBoletosPorEvento(){
+        
+        return null;
+        
+    }
+    
+}

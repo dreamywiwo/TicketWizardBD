d91b81d8d1c6bdf1802137c3803516eebdb3486e
LeonelCarballo <leonel.carballo235352@potros.itson.edu.mx>
2025-02-21 22:28:38 -0700

    Manejador de conexiones

--- src/main/java/itson/ticketwizard/persistencia/ManejadorConexiones.java
+++ src/main/java/itson/ticketwizard/persistencia/ManejadorConexiones.java
@@ -4,10 +4,28 @@
  */
 package itson.ticketwizard.persistencia;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author Dana Chavez
  */
 public class ManejadorConexiones {
+    // Informacion para conectar a la BD
+    private final String cadenaConexion = "jdbc:mysql://localhost/ticketWizard";
+    private final String usuario = "root";
+    private final String contrasenia = "Danoninocongelado1!";
+    
+    public Connection crearConexion() throws SQLException {
+        Connection conexion = DriverManager.getConnection(
+            cadenaConexion,
+            usuario,
+            contrasenia
+        );
+        return conexion;
+            
+    }
     
 }

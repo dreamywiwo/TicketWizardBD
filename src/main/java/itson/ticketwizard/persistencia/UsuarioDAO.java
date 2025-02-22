9f7a28e22dfac31e5bf3e9dd4539e950415327ba
LeonelCarballo <leonel.carballo235352@potros.itson.edu.mx>
2025-02-21 22:29:48 -0700

    ActualizarSaldo

--- src/main/java/itson/ticketwizard/persistencia/UsuarioDAO.java
+++ src/main/java/itson/ticketwizard/persistencia/UsuarioDAO.java
@@ -4,7 +4,13 @@
  */
 package itson.ticketwizard.persistencia;
 
+import itson.ticketwizard.entidades.Boleto;
 import itson.ticketwizard.entidades.Usuario;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -12,35 +18,57 @@ import java.util.List;
  * @author Dana Chavez
  */
 public class UsuarioDAO {
-    
-    public Usuario obtenerUsuarioPorCorreo (String correoElectronico) {
-        
+
+    public Usuario obtenerUsuarioPorCorreo(String correoElectronico) {
+
         return null;
-        
+
     }
-    
-    public Usuario obtenerUsuarioPorId (Integer idUsuario) {
-        
+
+    public Usuario obtenerUsuarioPorId(Integer idUsuario) {
+
         return null;
-        
+
     }
-    
-    public boolean verificarCredenciales (String correoElectronico, String contrasena) {
-        
+
+    public boolean verificarCredenciales(String correoElectronico, String contrasena) {
+
         return false;
-        
+
     }
-    
-    public boolean actualizarSaldo (Integer idUsuario, float nuevoSaldo) {
-        
-        return false;
-        
+
+    public void actualizarSaldo(Integer idUsuario, float nuevoSaldo) {
+
+        String codigoSQL = """
+                            UPDATE usuarios SET saldo = ? WHERE id = ? 
+                            ;
+                           """;
+
+        try {
+            Connection conexion = this.manejadorConexiones.crearConexion();
+            PreparedStatement comando = conexion.prepareStatement(codigoSQL);
+
+            comando.setFloat(1, nuevoSaldo);
+            comando.setInt(2, idUsuario);
+
+            ResultSet resultadosConsulta = comando.executeQuery();
+
+            int rowsAffected = comando.executeUpdate();
+            if (rowsAffected > 0) {
+                System.out.println("Saldo actualizado correctamente.");
+            } else {
+                System.out.println("Usuario no encontrado.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
-    
-    public List<Usuario> obtenerDatosUsuario (Integer idUsuario){
-    
+
+    public List<Usuario> obtenerDatosUsuario(Integer idUsuario) {
+
         return null;
-    
+
     }
-    
+
 }

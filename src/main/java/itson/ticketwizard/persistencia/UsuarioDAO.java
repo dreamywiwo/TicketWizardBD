d14e931416b61489a4fb1e187f806e77cc55090c
LeonelCarballo <leonel.carballo235352@potros.itson.edu.mx>
2025-02-22 00:46:01 -0700

    Update UsuarioDAO.java

--- src/main/java/itson/ticketwizard/persistencia/UsuarioDAO.java
+++ src/main/java/itson/ticketwizard/persistencia/UsuarioDAO.java
@@ -46,7 +46,7 @@ public class UsuarioDAO {
     public void actualizarSaldo(Integer idUsuario, float nuevoSaldo) {
 
         String codigoSQL = """
-                            UPDATE usuarios SET saldo = ? WHERE id = ? 
+                            UPDATE usuarios SET saldo = ? WHERE idUsuario = ? 
                             ;
                            """;
 

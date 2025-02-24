0149a6368e583083392fe51fffe88cadb9a33398
dreamywiwo <danacg11@hotmail.com>
2025-02-23 19:21:05 -0700

    Ventana apartado

--- src/main/java/itson/ticketwizard/presentacion/TransaccionPanel.java
+++ src/main/java/itson/ticketwizard/presentacion/TransaccionPanel.java
@@ -8,12 +8,12 @@ package itson.ticketwizard.presentacion;
  *
  * @author Dana Chavez
  */
-public class TransaccionesPanel extends javax.swing.JFrame {
+public class TransaccionPanel extends javax.swing.JFrame {
 
     /**
      * Creates new form TransaccionesPanel
      */
-    public TransaccionesPanel() {
+    public TransaccionPanel() {
         initComponents();
     }
 
@@ -96,20 +96,21 @@ public class TransaccionesPanel extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(TransaccionesPanel.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TransaccionPanel.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(TransaccionesPanel.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TransaccionPanel.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(TransaccionesPanel.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TransaccionPanel.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(TransaccionesPanel.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TransaccionPanel.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new TransaccionesPanel().setVisible(true);
+                new TransaccionPanel().setVisible(true);
             }
         });
     }
